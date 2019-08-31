825c67d3bb065b85fc93683f8a4dba8c6eedd2c9
Matthieu Dazy <matthieu.dazy@gmail.com>
2019-08-31 10:57:39 +0200

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
+import cors from "cors";
+
 import chats from "./chats";
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 app.get("/chats", (req, res) => res.send(chats));
 
