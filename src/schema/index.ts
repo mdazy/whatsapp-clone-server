6e6afbbea2c4e9317805b9dd4bf7ccbf576081ac
Matthieu Dazy <matthieu.dazy@gmail.com>
2019-08-31 11:37:24 +0200

    Fix typos and path

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,6 +2,6 @@ import { importSchema } from "graphql-import";
 import { makeExecutableSchema } from "graphql-tools";
 import resolvers from "./resolvers";
 
-const typeDefs = importSchema("schema/typeDefs.graphql");
+const typeDefs = importSchema("src/schema/typedefs.graphql");
 
 export default makeExecutableSchema({ resolvers, typeDefs });
