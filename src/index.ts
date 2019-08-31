b644c1b18816b066e3a5b0efddedd12fdd47957c
Matthieu Dazy <matthieu.dazy@gmail.com>
2019-08-31 11:40:37 +0200

    Setup Apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
+import { ApolloServer, gql } from "apollo-server-express";
 import express from "express";
-
 import cors from "cors";
 
-import chats from "./chats";
+import schema from "./schema";
+import chats from "./db";
 
 const app = express();
 const port = 4000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/chats", (req, res) => res.send(chats));
+
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/graphql" });
 
 app.listen(port, () => console.log("Server started on, port", port));
