32f779e267e064d67584bfe671ddd5673c7e464b
Matthieu Dazy <matthieu.dazy@gmail.com>
2019-09-26 14:30:45 +0200

    Add sendMessage mutation

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -32,13 +32,35 @@ const resolvers = {
     },
     chat(_: any, params: any) {
       const id = Number(params.id);
-      let chatRoom = chatRooms.filter(c => c.id === id);
+      const chatRoom = chatRooms.filter(c => c.id === id);
       if (chatRoom.length === 0) {
-        // hmmm which do we want?
-        // throw new Error(`No chat with id ${id}`);
-        return undefined;
+        throw new Error(`No chat with id ${id}`);
       }
       return { id };
+    }
+  },
+  Mutation: {
+    sendMessage(_: any, params: any) {
+      const chatId = Number(params.chatId);
+      const userId = Number(params.userId);
+      const chatRoom = chatRooms.filter(c => c.id == chatId);
+      if (chatRoom.length === 0) {
+        throw new Error(`No chat with id ${chatId}`);
+      }
+      const user = users.filter(u => u.id === userId);
+      if (user.length === 0) {
+        throw new Error(`No user with id ${userId}`);
+      }
+      const messageId = messages.length + 1;
+      const message = {
+        id: messages.length + 1,
+        fromUserId: userId,
+        content: params.content,
+        date: new Date(Date.now())
+      };
+      messages.push(message);
+      chatRoom[0].messageIds.push(message.id);
+      return message;
     }
   }
 };
