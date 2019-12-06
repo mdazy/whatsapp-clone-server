32d32041c451ecc5aec1f6fe8a2d44aa2805e1c5
Matthieu Dazy <matthieu.dazy@emerson.com>
2019-12-06 12:01:54 +0100

    Force resolvers type in schema (yuck?)

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,7 @@
 import { importSchema } from "graphql-import";
-import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema, IResolvers } from "graphql-tools";
 import resolvers from "./resolvers";
 
 const typeDefs = importSchema("src/schema/typedefs.graphql");
 
-export default makeExecutableSchema({ resolvers, typeDefs });
+export default makeExecutableSchema({ resolvers: resolvers as IResolvers, typeDefs });
